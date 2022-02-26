4b8c3f85babeff43608b87e125d4de9fa548ade1
Andrea Scarpa <scarpandrea.1996@hotmail.com>
2022-02-26 11:49:47 +0100

    add img and layout header and footer

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -1,3 +1,23 @@
-<header>
-    <h1>Header</h1>
+<header class="container">
+    <img src="{{asset('storage/img/Logo.png')}}" alt="">
+    @auth
+    <div>
+        <a class="btn btn-danger" href="{{ route('logout') }}">LOGOUT</a>
+        <a class="btn btn-primary" href="{{ route('create') }}">Carrello</a>
+    </div>
+    @endauth
+
+    @guest
+        <div>
+            <a href="">
+                <i class="fas fa-cart-arrow-down"></i>
+            </a>
+
+            <div id="accedi_btn">
+                <a  href="{{ route('register_page') }}">Accedi</a>
+            </div>
+        </div>
+     
+
+    @endguest
 </header>
