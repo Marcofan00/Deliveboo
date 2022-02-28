f0177011ba1feaf2ae873eca60f58420eb0f349a
Micaela87 <84184729+Micaela87@users.noreply.github.com>
2022-02-28 21:40:12 +0100

    registration handled

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use App\Providers\RouteServiceProvider;
-use App\User;
+use App\Models\User;
 use Illuminate\Foundation\Auth\RegistersUsers;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
@@ -50,9 +50,13 @@ class RegisterController extends Controller
     protected function validator(array $data)
     {
         return Validator::make($data, [
-            'name' => ['required', 'string', 'max:255'],
-            'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
+            'email' => ['required', 'string', 'email', 'max:60', 'unique:users'],
             'password' => ['required', 'string', 'min:8', 'confirmed'],
+            'full_name' => ['required', 'string', 'max:150'],
+            'restaurant_name' => ['required', 'string'],
+            'address' => ['required', 'string'],
+            'vat_number' => ['required', 'max:16'],
+            'logo' => ['image']
         ]);
     }
 
@@ -64,10 +68,37 @@ class RegisterController extends Controller
      */
     protected function create(array $data)
     {
-        return User::create([
-            'name' => $data['name'],
+        $newUserData = [
+            'full_name' => $data['full_name'],
             'email' => $data['email'],
             'password' => Hash::make($data['password']),
-        ]);
+            'restaurant_name' => $data['restaurant_name'],
+            'address' => $data['address'],
+            'vat_number' => $data['vat_number']
+        ];
+
+        if ($data['logo']) {
+
+            $imageFile = $data['logo'];
+
+            $fileName = rand(100000, 999999) . '_' . time().'.'.$request->logo->extension();
+
+            $imageFile -> storeAs('img', $fileName, 'public');
+
+            $newUserData['logo'] = $fileName;
+
+            return User::create($newUserData);
+
+        }
+
+        return User::create($newUserData);
+
+        
+
+    
+
+        
+        
+
     }
 }
