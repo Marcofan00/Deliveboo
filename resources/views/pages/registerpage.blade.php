d500cd0850f0d84d6bac742f74a6fc63136d685f
Marco Fanelli <86248724+Marcofan00@users.noreply.github.com>
2022-03-01 09:57:54 +0100

    login and register add

--- resources/views/pages/registerpage.blade.php
+++ resources/views/pages/registerpage.blade.php
@@ -1,23 +1,34 @@
 @extends('layouts.main-layout')
 @section('content')
     @guest
-    <h2>Registrazione</h2>
-    <form action="{{ route('register') }}" method="POST">
-        @method('POST')
-        @csrf
+    <div id="register">
+        <h2>Registrazione</h2>
+        <form action="{{ route('register') }}" method="POST">
+            @method('POST')
+            @csrf
 
-        <label for="name">Name</label><br>
-        <input type="text" name="name"><br>
-        <label for="email">E-mail</label><br>
-        <input type="text" name="email"><br>
-        <label for="password">Password</label><br>
-        <input type="password" name="password"><br>
-        <label for="password_confirmation">Password confirmation</label><br>
-        <input type="password" name="password_confirmation"><br>
-        <br>
-        <button type="submit" class="btn btn-success">REGISTER</button>
-    </form>
+            <label for="email">E-mail</label><br>
+            <input type="text" name="email"><br>
+            <label for="password">Password</label><br>
+            <input type="password" name="password"><br>
+            <label for="password_confirmation">Password confirmation</label><br>
+            <input type="password" name="password_confirmation"><br>
+            <label for="full_name">Name</label><br>
+            <input type="text" name="full_name"><br>
+            <label for="restaurant_name">Nome ristorante</label><br>
+            <input type="text" name="restaurant_name"><br>
+            <label for="address">Indirizzo</label><br>
+            <input type="text" name="address"><br>
+            <label for="vat_number">Partita IVA</label><br>
+            <input type="text" name="vat_number"><br>
+            <label for="logo">Logo</label><br>
+            <input type="file" name="logo"><br>
 
-    <a class="btn btn-secondary" href="{{url('/')}}">BACK TO HOME</a>
+            <button type="submit" class="btn btn-success">REGISTER</button>
+        </form>
+
+        <a class="btn btn-secondary" href="{{url('/')}}">BACK TO HOME</a>
+    </div>
+    
     @endguest
 @endsection
