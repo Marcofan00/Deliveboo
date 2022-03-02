1a6dec8ee865f8813684bb40a453847e198edb9e
Andrea Scarpa <scarpandrea.1996@hotmail.com>
2022-03-02 17:35:51 +0100

    hide food action

--- resources/js/components/DashboardComponent.vue
+++ resources/js/components/DashboardComponent.vue
@@ -42,7 +42,7 @@
                                  <div v-if="food.visible" id="nascondi" @click="hideCard(food.id)">
                                    <i class="far fa-eye-slash"></i> Hide  
                                 </div>
-                                <div v-else>
+                                <div v-else >
                                     <button @click="MakeVisibleFood(food.id)">Rendi dinyovo visibile</button>
                                 </div>
                             
@@ -81,7 +81,8 @@
             return {
                 foods_visibility : false,
                 foods : [],
-                orders_visibility : false
+                orders_visibility : false,
+                hidebtn : true,
             }
         },
         created(){
@@ -112,11 +113,56 @@
                 
             },
             hideCard(id){
-                
-               
+                    axios.post('http://localhost:8000/api/dashboard/delete/'+ id)
+                .then(res => {
+                    console.log(res.data);
+                    (res.data);
+                    // let food = res.data;
+                    // console.log(food.id);
+                    // for ( let i = 0 ; i < this.foods.length; i++){
+                    //     element = this.foods[i];
+                        
+                    //     if( element.id === id){
+                    //         element.visible = 0;
+                    //         element = food;
+                    //     }
+                    // }
+
+
+                }).catch(err=>{
+                    console.error(err);
+                });
+                // let data = id;
+                // try {
+                //     let response = await fetch('http://localhost:8000/api/dashboard/delete/'+ id,{
+                //         method : 'POST',
+                //         body : data
+                //     })
+                // } catch (err) {
+                //     console.log(err);
+                // }
             },
             MakeVisibleFood(id){
+                 axios.post('http://localhost:8000/api/dashboard/delete/'+ id)
+                .then(res => {
 
+                    let food = res.data;
+                    console.log(food);
+                    // console.log(food.id);
+                    // for ( let i = 0 ; i < this.foods.length; i++){
+                    //     element = this.foods[i];
+                        
+                    //     if( element.id === id){
+                    //         element.visible = 1;
+                    //         element = food;
+                    //     }
+                    // }
+
+
+
+                }).catch(err=>{
+                    console.error(err);
+                });
             }
         }
     }
