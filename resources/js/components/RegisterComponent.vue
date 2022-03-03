aacbbade29af2aafba2c7ddae26af0210682c3bf
Micaela87 <84184729+Micaela87@users.noreply.github.com>
2022-03-03 23:38:34 +0100

    RegisterComponent form data validation completed

--- resources/js/components/RegisterComponent.vue
+++ resources/js/components/RegisterComponent.vue
@@ -2,34 +2,60 @@
     <div id="register">
         <h2>Registrazione</h2>
         <form>
+
             <label for="email">E-mail</label><br>
-            <input type="text" name="email" v-model="email"><br>
+            <input type="email" name="email" v-model.trim="email" required :class="errors.emailError ? 'red' : 'green' "><br>
+            <div v-if="errors.emailError">{{ errors.emailError }}</div>
+
             <label for="password">Password</label><br>
-            <input type="password" name="password" v-model="password"><br>
+            <input type="password" name="password" v-model.trim="password" required><br>
+            <div v-if="errors.passwordError">{{ errors.passwordError }}</div>
+
             <label for="password_confirmation">Conferma Password</label><br>
-            <input type="password" name="password_confirmation" v-model="passwordConfirmed"><br>
+            <input type="password" name="password_confirmation" v-model.trim="passwordConfirmed" required><br>
+            <div v-if="errors.passwordConfirmError">{{ errors.passwordConfirmError }}</div>
+
             <label for="firstame">Nome</label><br>
-            <input type="text" name="firstname" v-model="firstName"><br>
+            <input type="text" name="firstname" v-model.trim="firstName" required><br>
+            <div v-if="errors.firstNameError">{{ errors.firstNameError }}</div>
+
             <label for="lastname">Cognome</label><br>
-            <input type="text" name="lastname" v-model="lastName"><br>
+            <input type="text" name="lastname" v-model.trim="lastName" required><br>
+            <div v-if="errors.lastNameError">{{ errors.lastNameError }}</div>
+
             <label for="restaurant_name">Nome Ristorante</label><br>
-            <input type="text" name="restaurant_name" v-model="restaurantName"><br>
+            <input type="text" name="restaurant_name" v-model.trim="restaurantName" required><br>
+            <div v-if="errors.restaurantNameError">{{ errors.restaurantNameError }}</div>
+
             <label for="street">Indirizzo Ristorante</label><br>
-            <input type="text" name="street" v-model="restaurantAddress"><br>
+            <input type="text" name="street" v-model.trim="restaurantAddress" required><br>
+            <div v-if="errors.restaurantAddressError">{{ errors.restaurantAddressError }}</div>
+
             <label for="streetnumber">Numero Civico</label><br>
-            <input type="number" name="streetnumber" v-model="streetNumber"><br>
+            <input type="number" name="streetnumber" v-model.trim="streetNumber" required><br>
+            <div v-if="errors.streetNumberPostalCordeError">{{ errors.streetNumberPostalCordeError }}</div>
+
             <label for="postalcode">CAP</label><br>
-            <input type="number" name="postalcode" v-model="postalCode"><br>
+            <input type="number" name="postalcode" v-model.trim="postalCode" required><br>
+            <div v-if="errors.streetNumberPostalCordeError">{{ errors.streetNumberPostalCordeError }}</div>
+
             <label for="city">Città</label><br>
-            <input type="text" name="city" v-model="city"><br>
+            <input type="text" name="city" v-model.trim="city" required><br>
+            <div v-if="errors.cityError">{{ errors.cityError }}</div>
+
             <label for="vat_number">Partita IVA</label><br>
-            <input type="text" name="vat_number" v-model="vatNumber">
+            <input type="text" name="vat_number" v-model.trim="vatNumber" required>
+            <div v-if="errors.vatNumberError">{{ errors.vatNumberError }}</div>
+
             <div v-for="category in categories" :key="category.id">
                 <label for="categories">{{ category.name }}</label>
-                <input type="checkbox" :value="category.id" v-model="categoriesChecked">
+                <input type="checkbox" :value="category.id" v-model="categoriesChecked" required>
             </div>
+            <div v-if="errors.categoryError">{{ errors.categoryError }}</div>
+
             <label for="logo">Logo</label><br>
             <input type="file" name="logo" @change="getFile"><br>
+            <div v-if="errors.fileError">{{ errors.fileError }}</div>
 
             <button type="button" class="btn btn-success" @click="sendData">REGISTER</button>
         </form>
@@ -55,7 +81,22 @@
                 streetNumber: '',
                 postalCode: '',
                 city: '',
-                vatNumber: ''
+                vatNumber: '',
+                errors: {
+                    emailError: '',
+                    fileError: '',
+                    passwordError: '',
+                    passwordConfirmError: '',
+                    firstNameError: '',
+                    lastNameError: '',
+                    restaurantNameError: '',
+                    restaurantAddressError: '',
+                    streetNumberPostalCordeError: '',
+                    cityError: '',
+                    vatNumberError: '',
+                    categoryError: ''
+                },
+                namesRegex: /^[a-z ,.\'-]+$/gi
             }
         },
         mounted() {
@@ -63,12 +104,56 @@
         },
         computed: {
             fullName() {
-                // console.log(this.firstName, this.lastName);
                 return this.firstName + ' ' + this.lastName;
             },
             fullAddress() {
-                // console.log(this.restaurantAddress + ', ' + this.streetNumber + ', ' + this.postalCode + ', ' + this.city);
                 return this.restaurantAddress + ', ' + this.streetNumber + ', ' + this.postalCode + ', ' + this.city;
+            }
+        },
+        watch: {
+            email(value) {
+                this.email = value;
+                this.validateEmail(value);
+            },
+            password(value) {
+                this.password = value;
+                this.validatePassword(value);
+            },
+            passwordConfirmed(value) {
+                this.passwordConfirmed = value;
+                this.validatePasswordConfirm(value);
+            },
+            firstName(value) {
+                this.firstName = value;
+                this.validateFirstName(value);
+            },
+            lastName(value) {
+                this.lastName = value;
+                this.validateLastName(value);
+            },
+            restaurantName(value) {
+                this.restaurantName = value;
+                this.validateRestaurantName(value);
+            },
+            restaurantAddress(value) {
+                this.restaurantAddress = value;
+                this.validateRestaurantAddress(value);
+            },
+            streetNumber(value) {
+                this.streetNumber = value;
+                this.validateStreetNumberPostalCode(value);
+            },
+            postalCode(value) {
+                this.postalCode = value;
+                this.validateStreetNumberPostalCode(value);
+            },
+            city(value) {
+                this.city = value;
+                this.validateCity(value);
+            },
+            vatNumber(value) {
+                this.vatNumber = value;
+                this.validateVatNumber(value);
             }
         },
         methods: {
@@ -92,7 +177,21 @@
             },
             sendData: async function() {
 
-                if (this.validateFormData()) {
+                if (
+                    this.validateEmail(this.email) &&
+                    this.validateCategory() &&
+                    this.validateCity(this.city) &&
+                    this.validateFileType() &&
+                    this.validateFirstName(this.firstName) &&
+                    this.validateLastName(this.lastName) &&
+                    this.validatePassword(this.password) &&
+                    this.validatePasswordConfirm(this.passwordConfirmed) &&
+                    this.validateRestaurantAddress(this.restaurantAddress) &&
+                    this.validateRestaurantName(this.restaurantName) &&
+                    this.validateStreetNumberPostalCode(this.streetNumber) &&
+                    this.validateStreetNumberPostalCode(this.postalCode) &&
+                    this.validateVatNumber(this.vatNumber)
+                ) {
                     let data = new FormData();
 
                     data.append('categories', this.categoriesChecked);
@@ -124,60 +223,137 @@
                 }
 
             },
-            validateFormData() {
+            validateEmail(email) {
 
-                let namesRegex = '/^[a-z ,.\'-]+$/i';
-
-                if (!this.email.includes('/^([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5})$/')) {
+                if (!/^([a-zA-Z0-9\_\-\.]+)@([a-zA-Z0-9\_\-\.]+)\.([a-zA-Z]{2,5})$/g.test(email)) {
+                    this.errors.emailError = 'Email non valida';
                     return false;
                 }
 
-                if (!this.password.includes('/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/')) {
+                this.errors.emailError = '';
+                return true;
+            },
+            validatePassword(password) {
+
+                if (!/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/g.test(password)) {
+                    this.errors.passwordError = 'Password non valida';
                     return false;
                 }
 
-                if (this.passwordConfirmed !== this.password) {
+                this.errors.passwordError = '';
+                return true;
+            },
+            validatePasswordConfirm(passwordConfirmed) {
+
+                if (passwordConfirmed !== this.password) {
+                    this.errors.passwordConfirmError = 'Questa password non coirrisponde alla prima inserita';
                     return false;
                 }
 
-                if (!this.firstName.includes(namesRegex)) {
+                this.errors.passwordConfirmError = '';
+                return true;
+            },
+            validateFirstName(firstName) {
+
+                if (!this.namesRegex.test(firstName)) {
+                    this.errors.firstNameError = 'Nome non valido';
                     return false;
                 }
 
-                if (!this.lastName.includes(namesRegex)) {
+                this.errors.firstNameError = '';
+                return true;
+            },
+            validateLastName(lastName) {
+
+                if (!this.namesRegex.test(lastName)) {
+                    this.errors.lastNameError = 'Cognome non valido';
                     return false;
                 }
 
-                if (!this.restaurantName.includes(namesRegex)) {
+                this.errors.lastNameError = '';
+                return true;                
+            },
+            validateRestaurantName(restaurantName) {
+
+                if (!/^[-a-z ,.\'0-9]+$/gi.test(restaurantName)) {
+                    this.errors.restaurantNameError = 'Nome ristorante non valido';
                     return false;
                 }
 
-                if (!this.restaurantAddress.includes(namesRegex)) {
+                this.errors.restaurantNameError = '';
+                return true;                 
+            },
+            validateRestaurantAddress(restaurantAddress) {
+
+                if (!/^[-a-z ,.\'0-9]+$/gi.test(restaurantAddress)) {
+                    this.errors.restaurantAddressError = 'Indirizzo non valido';
                     return false;
                 }
 
-                if (typeof this.streetNumber !== 'Number' || typeof this.postalCode !== 'Number') {
+                this.errors.restaurantAddressError = '';
+                return true;  
+            },
+            validateStreetNumberPostalCode(number) {
+
+                if (typeof Number(number) !== 'number') {
+                    this.errors.streetNumberPostalCordeError = 'Numero civico o CAP non validi';
                     return false;
                 }
 
-                if (!this.city.includes(namesRegex)) {
+                    this.errors.streetNumberPostalCordeError = '';
+                    return true;                
+            },
+            validateCity(city) {
+
+                if (!this.namesRegex.test(city)) {
+                    this.errors.cityError = 'Città non valida';
                     return false;
                 }
 
-                if (!this.vatNumber.includes('/it\d{14}/i')) {
+                this.errors.cityError = '';
+                return true;                 
+            },
+            validateVatNumber(vatNumber) {
+
+                if (!/^it\d{14}$/gi.test(vatNumber)) {
+                    this.errors.vatNumberError = 'Partita IVA non valida. Formato richiesto: IT seguito da 14 cifre';
                     return false;
                 }
+
+                this.errors.vatNumberError = '';
+                return true;
+            },
+            validateFileType() {
 
                 if (!this.file.type.includes('/image/')) {
+                    this.errors.fileError = 'Formato file non valido. Inserisci una immagine';
                     return false;
                 }
+
+                this.errors.fileError = '';
+                return true;
+            },
+            validateCategory() {
 
                 if (!this.categoriesChecked) {
+                    this.errors.categoryError = 'Devi scegliere almeno una categoria';
                     return false;
                 }
 
+                this.errors.categoryError = '';
                 return true;
             }
         }
     }
 </script>
+
+<style scoped>
+
+    .red {
+        border-color: red;
+    }
+
+    .green {
+        border-color: green;
+    }
+</style>
