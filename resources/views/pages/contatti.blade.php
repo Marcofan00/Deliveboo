0c2530f565943fba1af16b2324b5c3902fc8c311
Andrea Scarpa <scarpandrea.1996@hotmail.com>
2022-03-13 16:10:10 +0100

    add marco  contatti

--- resources/views/pages/contatti.blade.php
+++ resources/views/pages/contatti.blade.php
@@ -15,16 +15,62 @@
                 </div>
     
                 <div class="descrizione_contatto">
-                    <div>
+                    <div class="contact">
                         <i class="icon fas fa-phone-alt"></i> : 3466206298
                     </div>
-                    <div>
+                    <div class="contact">
                         <i class="icon fas fa-envelope"></i> : scarpandrea.1996@hotmail.com
                     </div>
-                    <div>
+                    <div class="contact">
+                        <i class="icon fab fa-linkedin"></i> : <a href="https://bit.ly/3J7hmoN">https://bit.ly/3J7hmoN</a>
+                    </div>
+                    <div class="contact">
+                        <i class="icon fab fa-github"></i> :  <a href="https://github.com/Andrea1302">https://github.com/Andrea1302</a>
+                    </div>
+                </div>
+            </div>
+    
+            <div class="contatto">
+                <div class="img_contatto">
+                    <img src="{{asset('storage/img/marco.jpg')}}" alt="">
+                    <h2>Marco Fanelli</h2>
+    
+                </div>
+    
+                <div class="descrizione_contatto">
+                    <div class="contact">
+                        <i class="icon fas fa-phone-alt"></i> : 3456133077
+                    </div>
+                    <div class="contact">
+                        <i class="icon fas fa-envelope"></i> : marcofan00@gmail.com
+                    </div>
+                    <div class="contact">
+                        <i class="icon fab fa-linkedin"></i> : <a href="https://bit.ly/3KzK4z8">https://bit.ly/3KzK4z8</a>
+                    </div>
+                    <div class="contact">
+                        <i class="icon fab fa-github"></i> : <a href="https://github.com/Marcofan00">https://github.com/Marcofan00</a>
+                    </div>
+                </div>
+            </div>
+    
+            <div class="contatto">
+                <div class="img_contatto">
+                    <img src="{{asset('storage/img/io.jpg')}}" alt="">
+                    <h2>Andrea Scarpa</h2>
+    
+                </div>
+    
+                <div class="descrizione_contatto">
+                    <div class="contact">
+                        <i class="icon fas fa-phone-alt"></i> : 3466206298
+                    </div>
+                    <div class="contact">
+                        <i class="icon fas fa-envelope"></i> : scarpandrea.1996@hotmail.com
+                    </div>
+                    <div class="contact">
                         <i class="icon fab fa-linkedin"></i> : <a href="https://github.com/Andrea1302">https://github.com/Andrea1302</a>
                     </div>
-                    <div>
+                    <div class="contact">
                         <i class="icon fab fa-github"></i> : <a href="https://bit.ly/3J7hmoN">https://bit.ly/3J7hmoN</a>
                     </div>
                 </div>
@@ -38,16 +84,16 @@
                 </div>
     
                 <div class="descrizione_contatto">
-                    <div>
+                    <div class="contact">
                         <i class="icon fas fa-phone-alt"></i> : 3466206298
                     </div>
-                    <div>
+                    <div class="contact">
                         <i class="icon fas fa-envelope"></i> : scarpandrea.1996@hotmail.com
                     </div>
-                    <div>
+                    <div class="contact">
                         <i class="icon fab fa-linkedin"></i> : <a href="https://github.com/Andrea1302">https://github.com/Andrea1302</a>
                     </div>
-                    <div>
+                    <div class="contact">
                         <i class="icon fab fa-github"></i> : <a href="https://bit.ly/3J7hmoN">https://bit.ly/3J7hmoN</a>
                     </div>
                 </div>
@@ -61,62 +107,16 @@
                 </div>
     
                 <div class="descrizione_contatto">
-                    <div>
+                    <div class="contact">
                         <i class="icon fas fa-phone-alt"></i> : 3466206298
                     </div>
-                    <div>
+                    <div class="contact">
                         <i class="icon fas fa-envelope"></i> : scarpandrea.1996@hotmail.com
                     </div>
-                    <div>
+                    <div class="contact">
                         <i class="icon fab fa-linkedin"></i> : <a href="https://github.com/Andrea1302">https://github.com/Andrea1302</a>
                     </div>
-                    <div>
-                        <i class="icon fab fa-github"></i> : <a href="https://bit.ly/3J7hmoN">https://bit.ly/3J7hmoN</a>
-                    </div>
-                </div>
-            </div>
-    
-            <div class="contatto">
-                <div class="img_contatto">
-                    <img src="{{asset('storage/img/io.jpg')}}" alt="">
-                    <h2>Andrea Scarpa</h2>
-    
-                </div>
-    
-                <div class="descrizione_contatto">
-                    <div>
-                        <i class="icon fas fa-phone-alt"></i> : 3466206298
-                    </div>
-                    <div>
-                        <i class="icon fas fa-envelope"></i> : scarpandrea.1996@hotmail.com
-                    </div>
-                    <div>
-                        <i class="icon fab fa-linkedin"></i> : <a href="https://github.com/Andrea1302">https://github.com/Andrea1302</a>
-                    </div>
-                    <div>
-                        <i class="icon fab fa-github"></i> : <a href="https://bit.ly/3J7hmoN">https://bit.ly/3J7hmoN</a>
-                    </div>
-                </div>
-            </div>
-    
-            <div class="contatto">
-                <div class="img_contatto">
-                    <img src="{{asset('storage/img/io.jpg')}}" alt="">
-                    <h2>Andrea Scarpa</h2>
-    
-                </div>
-    
-                <div class="descrizione_contatto">
-                    <div>
-                        <i class="icon fas fa-phone-alt"></i> : 3466206298
-                    </div>
-                    <div>
-                        <i class="icon fas fa-envelope"></i> : scarpandrea.1996@hotmail.com
-                    </div>
-                    <div>
-                        <i class="icon fab fa-linkedin"></i> : <a href="https://github.com/Andrea1302">https://github.com/Andrea1302</a>
-                    </div>
-                    <div>
+                    <div class="contact">
                         <i class="icon fab fa-github"></i> : <a href="https://bit.ly/3J7hmoN">https://bit.ly/3J7hmoN</a>
                     </div>
                 </div>
