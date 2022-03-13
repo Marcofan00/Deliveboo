1f7b12d85d2790e0952e4eebed0aaa9bad70c08c
Andrea Scarpa <scarpandrea.1996@hotmail.com>
2022-03-13 17:02:17 +0100

    fix layout mail

--- resources/views/pages/order.blade.php
+++ resources/views/pages/order.blade.php
@@ -10,9 +10,8 @@
         *{
             box-sizing: border-box;
         }
-        .contenitore{
-            width: 40%;
-            height: 100vh;
+        .contenitore_mail{
+            width: 80%;
             margin: 0 auto;
             background-color: #009FB7;
             color: white;
@@ -20,65 +19,71 @@
             position: relative;
         }
         
-        ul{
+        #ul_mail{
             margin: 0;
             padding: 0;
+            display: flex;
+            flex-direction: column
         }
-        .logo{
+        .logo_mail{
             padding: 20px 0;
             width: 40%;
             margin: 0 auto;
             text-align: center
         }
-        li{
+        .li_mail{
             list-style: none;
             margin: 8px 0;
         }
         #ringraziamento{
-            position: absolute;
-            bottom: 20px;
-            right: 20px;
-            font-size: .7rem;
+            text-align: end;
+            padding-right: 16px;
+            margin-top: 64px;
+        }
+        #tot{
+            flex-grow: 1
         }
         @media all and ( max-width:576px){
-            .logo{
+            .logo_mail{
                 width: 100%;
             }
-            .contenitore{
+            .contenitore_mail{
                 width: 100%;
             }
         }
     </style>
 </head>
 <body>
-    <div class="logo">
+    <div class="logo_mail">
         <img src="{{asset('/storage/img/Logo.png')}}" alt="logo">
     </div>
-    <div class="contenitore">
+    <div class="contenitore_mail">
         
         <h2>Mail di conferma per l ordine effettutato</h2>
     
-        <ul>
-            <li>
+        <ul id="ul_mail">
+            <li class="li_mail">
                 Ordine Inviato da : {{$order -> buyer_fullname}}
             </li>
     
-            <li>
+            <li class="li_mail">
                 Indirizzo : {{$order -> buyer_address}}
             </li>
     
-            <li>
+            <li class="li_mail">
                 Transazione : avventuta con successo !
             </li>
     
-            <li>
+            <li class="li_mail" id="tot">
                 Totale ordine : {{$order -> bill}} &euro;
             </li>
+
+            <div id="ringraziamento">
+                Lo staff di DeeBoo ti ringrazia , Buon appetito {{$order -> buyer_fullname}}
+            </div>   
         </ul>
         
-        <div id="ringraziamento">
-            Lo staff di DeeBoo ti ringrazia , Buon appetito {{$order -> buyer_fullname}}
-        </div>    
+        
     </div>
     
     {{-- {{$order}} --}}
